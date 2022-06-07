f8c4e49475261af08297339069e07d247d4f2a3a
DebiprasadXD <debiprasaddaskdv@gmail.com>
2022-06-07 11:47:15 +0530

    Fixed some errors related with bunkr scraper

--- helpers/bunkr_scraper.py
+++ helpers/bunkr_scraper.py
@@ -1,3 +1,4 @@
+from operator import length_hint
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -13,20 +14,23 @@ def bunkr_scraper(url):
             "https": https_proxy })
 
     soup = BeautifulSoup(r.text,'html.parser')
-    soup2 = soup.find_all("script")
-    for i in soup2[11]:
-        f = json.loads(i)
-    
-    
-    for key , items in f.items():
-        if key == "props":
-            item = items.get("pageProps")
-            item2 = item.get("files")
-            length = len(item2)
+    script = soup.find("script", {"type":"application/json"})
+
     url_list = []
-    for i in range(length):
-        name = item2[i].get('name')
-        url = "https://media-files.bunkr.is/" + name
-        url_list.append(url)
-    return url_list
+    for i in script:
+        f = json.loads(i)
+        print(f)
+        for key , items in f.items():
+            if key == "props":
+                item = items.get("pageProps")
+                print(item)
+                item2 = item.get("files")
+                length = len(item2)
+                print(length_hint)                
+                for i in range(length):
+                    name = item2[i].get('name')
+                    url = "https://media-files.bunkr.is/" + name
+                    print(url)
+                    url_list.append(url)
+                return url_list
 
--- thumbnail/thumb.py
+++ thumbnail/thumb.py
@@ -8,14 +8,15 @@ os.makedirs("thumbnail",exist_ok=True)
 save_path = os.path.join("thumbnail")
 def make_thumbnail():
     try:
+        print("Processing Thumbnail")
         clip = VideoFileClip(source_path)
         duration = clip.duration
         thumbnail_duration = int(duration)//3
         frame = clip.get_frame(thumbnail_duration)
         thumbnail_path = os.path.join(save_path,"thumb.jpg")
         thumbnail = Image.fromarray(frame)
-        print("Processing Thumbnail")
         thumbnail.save(thumbnail_path)
+        print("Done")
         return thumbnail_path
     except :
         print("No video found")
