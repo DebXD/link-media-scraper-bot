cf8e9afd002559aba39972fe8cf5308dfd4094c5
DebiprasadXD <debiprasaddaskdv@gmail.com>
2023-02-08 21:16:37 +0530

    update requirements.txt

--- helpers/bunkr_scraper.py
+++ helpers/bunkr_scraper.py
@@ -1,27 +1,23 @@
 from bs4 import BeautifulSoup
-from decouple import config
 import asyncio
 import aiohttp
 
-http_proxy = config('HTTP_PROXY')
-https_proxy = config('HTTPS_PROXY')
 
-async def bunkr_scraper(url):
+def bunkr_scraper(url):
 
     async def get_text(url):
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as res:
                 return await res.text()
 
-
     async def res(url):
-    
+
         html_content = await get_text(url)
 
         soup = BeautifulSoup(html_content, "html.parser")
         url_list = []
         for link in soup.find_all("a"):
-            href = link.get("href") 
+            href = link.get("href")
             if len(href) > 30:
                 if "/v/" in href:
                     modified_href = "https://bunkr.su" + href
